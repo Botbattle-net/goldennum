a92590da461608d287c10789f3094ccacf4f0c93
KuangHongShen <171250560@smail.nju.edu.cn>
2018-09-18 19:56:52 +0800

    follow comments

--- goldennum.py
+++ goldennum.py
@@ -18,6 +18,8 @@ secretKey = sys.argv[1]
 roomid = sys.argv[2]
 sleeptime = int(sys.argv[3])
 real_user_num = 0
+listBestUsers = []
+listWorstUsers = []
 
 class MyThread(Thread):
     def __init__(self, event):
@@ -41,6 +43,7 @@ class User:
         self.userAct1 = userAct1
         self.userAct2 = userAct2
 
+
 def getAct():
     global goldNum
     global closeUser
@@ -48,9 +51,9 @@ def getAct():
     global userNum
     global listUser
     global real_user_num
-    #print('Hello Timer1!')
+    # print('Hello Timer1!')
 
-    #send request
+    # send request
     requestData = {"roomid":roomid, "key":secretKey}
     # print(requestData)
     # values = requests.get(url+"/getAct/",params=requestData)
@@ -72,7 +75,8 @@ def getAct():
             userAct2=user.get("userAct")[1]
         ))
         if user.get("userAct")[0] == 0 and user.get("userAct")[1] == 0:
-            real_user_num = real_user_num-1
+            if real_user_num > 0:
+                real_user_num = real_user_num-1
 
     total = 0
     for user in listUser:
@@ -87,19 +91,35 @@ def getAct():
     far_num = goldNum  # 离黄金数最远的
 
     for user in listUser:
-        if real_user_num > 2:
+        if real_user_num > 2:   # 寻找BestNum和WorstNum
             if abs(user.userAct1 - goldNum) < abs(close_num - goldNum):
                 close_num = user.userAct1
-                closeUser = user.userName
+                # closeUser = user.userName
             if abs(user.userAct2 - goldNum) < abs(close_num - goldNum):
                 close_num = user.userAct2
-                closeUser = user.userName
+                # closeUser = user.userName
             if abs(user.userAct1 - goldNum) > abs(far_num - goldNum):
                 far_num = user.userAct1
-                farUser = user.userName
+                # farUser = user.userName
             if abs(user.userAct2 - goldNum) > abs(far_num - goldNum):
                 far_num = user.userAct2
-                farUser = user.userName
+                # farUser = user.userName
+    for user in listUser:
+        if real_user_num > 2:
+            if user.userAct1 == close_num or user.userAct2 == close_num:    # 输出close_num的用户
+                listBestUsers.append(user.userName)
+            if user.userAct1 == far_num or user.userAct2 == far_num:        # 输出far_num的用户
+                listWorstUsers.append(user.userName)
+    # 清除重复
+    for user1 in listWorstUsers:
+        for user2 in listWorstUsers:
+            if user1 == user2:
+                listWorstUsers.remove(user2)
+    for user1 in listBestUsers:
+        for user2 in listBestUsers:
+            if user1 == user2:
+                listBestUsers.remove(user2)
+
     # for test
     # print(goldNum)
     # print(closeUser,close_num)
@@ -110,14 +130,14 @@ def getAct():
 
 
 def submitResult():
-    requestData = {"roomid":roomid, "key":secretKey}
-    data = {"roundTime":sleeptime, "goldenNum": goldNum, "userNum": userNum, "users":[]}
+    requestData = {"roomid": roomid, "key": secretKey}
+    data = {"roundTime": sleeptime, "goldenNum": goldNum, "userNum": userNum, "users": []}
 
     if real_user_num > 2:
         for user in listUser:
-            if user.userName == closeUser:
-                data.get("users").append({"userName":user.userName, "userScore":userNum - 2})
-            elif user.userName == farUser:
+            if user in listBestUsers:
+                data.get("users").append({"userName": user.userName, "userScore": userNum - 2})
+            elif user in listWorstUsers:
                 data.get("users").append({"userName": user.userName, "userScore": -2})
             else:
                 data.get("users").append({"userName": user.userName, "userScore": 0})
@@ -140,4 +160,4 @@ if __name__ == "__main__":
     thread.start()
 
     # this will stop the timer
-    #stopFlag.set()
+    # stopFlag.set()
